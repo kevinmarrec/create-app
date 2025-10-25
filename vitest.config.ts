b7c8a91a81a5f1dfff1cabfc89f4009c7fc38265
Kevin Marrec <kevin@marrec.io>
2025-10-25 16:07:02 +0200

    chore: file coverage mode all

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    reporters: ['verbose', 'github-actions'],
+    reporters: ['verbose'],
     coverage: {
       include: ['src/**/*.ts'],
       reporter: ['text', 'text-summary', 'json', 'json-summary'],
